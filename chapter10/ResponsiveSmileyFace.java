6f9e846e07003fd9d5f0143beaebe122866d4ac4
kevinkaiyenmou <51729662+kevinkaiyenmou@users.noreply.github.com>
2020-12-05 16:31:20 -0800

    Completed Responsive

--- chapter10/ResponsiveSmileyFace.java
+++ chapter10/ResponsiveSmileyFace.java
@@ -15,12 +15,12 @@ import javafx.stage.Stage;
 
 public class ResponsiveSmileyFace extends Application {
     final static double FACE_X = 125f, FACE_Y = 125f, FACE_RADIUS = 80f, FACE_EYE_LEFT_X = 86f, FACE_EYE_Y = 100f, FACE_EYE_RADIUS = 10f,
-    FACE_EYE_RIGHT_X = FACE_X + (FACE_X - FACE_EYE_LEFT_X), MOUTH_WIDTH = 25f, radiusX = 45f, radiusY = 35f,
+    FACE_EYE_RIGHT_X = FACE_X + (FACE_X - FACE_EYE_LEFT_X), MOUTH_WIDTH = 25f, SMILE_RADIUS_X = 45f, SMILE_RADIUS_Y = 35f,
     SCENE_HEIGHT = 2f * FACE_Y + 25f, SCENE_WIDTH = 2 * FACE_X, FONT_HEIGHT = 15f, CAPTION_X = FACE_RADIUS,
     CAPTION_Y = FACE_RADIUS + FACE_Y + 35f, ORIG_FACE_HRATIO = (FACE_RADIUS / SCENE_HEIGHT), ORIG_FACE_WRATIO = (FACE_RADIUS / SCENE_WIDTH), 
     ORIG_EYE_HORIZ_RATIO = (FACE_X - FACE_EYE_LEFT_X) / FACE_RADIUS, ORIG_EYE_VERT_RATIO = (FACE_Y - FACE_EYE_Y) / FACE_RADIUS, 
-    EYE_TO_FACE_RADIUS_RATIO = FACE_EYE_RADIUS / FACE_RADIUS, MOUTH_TO_FACE_RADIUS_X_RATIO = radiusX / FACE_RADIUS, 
-    MOUTH_TO_FACE_RADIUS_Y_RATIO = radiusY / FACE_RADIUS, FONT_TO_FACE_RATIO = FONT_HEIGHT / FACE_RADIUS;
+    EYE_TO_FACE_RADIUS_RATIO = FACE_EYE_RADIUS / FACE_RADIUS, MOUTH_TO_FACE_RADIUS_X_RATIO = SMILE_RADIUS_X / FACE_RADIUS, 
+    MOUTH_TO_FACE_RADIUS_Y_RATIO = SMILE_RADIUS_Y / FACE_RADIUS, FONT_TO_FACE_RATIO = FONT_HEIGHT / FACE_RADIUS;
     //what is MOUTH_WIDTH? why is it 25. Is it the width of the circle that the arc makes?
 
 /*     private static boolean captionSizeTooLarge(Text captionToCheck, Scene sceneToCheck, Circle faceToCheck){
@@ -44,7 +44,7 @@ public class ResponsiveSmileyFace extends Application {
         rightEye.setStroke(Color.BLUE);
 
         // create and configure a smiling mouth
-        final Arc mouth = new Arc(FACE_X, FACE_X + MOUTH_WIDTH, radiusX, radiusY, 0, -180);
+        final Arc mouth = new Arc(FACE_X, FACE_Y + (FACE_RADIUS - SMILE_RADIUS_Y)/2, SMILE_RADIUS_X, SMILE_RADIUS_Y, 0, -180);
         mouth.setFill(Color.YELLOW);
         mouth.setStroke(Color.BLUE);
         mouth.setType(ArcType.OPEN);
@@ -67,7 +67,7 @@ public class ResponsiveSmileyFace extends Application {
         
         // How to listen resize event of Stage in JavaFX?
         // https://stackoverflow.com/a/38216917/1757756
-        ChangeListener<Number> stageWidthListener = (observable, oldValue, newValue) -> {
+        ChangeListener<Number> stageListener = (observable, oldValue, newValue) -> {
             System.out.println("Stage height: " + stage.getHeight() + " Stage width: " + stage.getWidth());
             //'face' width and height
             //'rightEye' width and height
@@ -97,9 +97,9 @@ public class ResponsiveSmileyFace extends Application {
             
             
             mouth.setRadiusX(MOUTH_TO_FACE_RADIUS_X_RATIO * newFaceRadius); 
-            mouth.setRadiusY(MOUTH_TO_FACE_RADIUS_Y_RATIO * newFaceRadius); 
-
-            
+            mouth.setRadiusY(MOUTH_TO_FACE_RADIUS_Y_RATIO * newFaceRadius);
+            mouth.setCenterX(newFaceX);
+            mouth.setCenterY(newFaceY + (newFaceRadius - mouth.getRadiusY())/2);
             face.setRadius(newFaceRadius);
             face.setCenterX(newFaceX);
             face.setCenterY(newFaceY);
@@ -130,53 +130,14 @@ public class ResponsiveSmileyFace extends Application {
             System.out.println("Caption height: " + caption.getLayoutBounds().getHeight() + " Caption width: " + caption.getLayoutBounds().getWidth() + " Caption font: " + caption.getFont());
             // not needed: 'scene' width and height
         };
-        ChangeListener<Number> stageHeightListener = (observable, oldValue, newValue) -> {
-            System.out.println("Stage height: " + stage.getHeight() + " Stage width: " + stage.getWidth());
-            //'face' width and height
-            //'rightEye' width and height
-            //'leftEye' width and height
-            //'mouth' width and height
-            //'caption' positionx, positiony, font size
-            
-            // Text captionWithNewFont = new Text(FACE_RADIUS, FACE_RADIUS + FACE_Y + 35, "Smiley Face");
-            double newFaceRadius = stage.getWidth() > stage.getHeight() ? stage.getHeight() * ORIG_FACE_HRATIO : stage.getWidth() * ORIG_FACE_WRATIO;
-            double newFaceX = stage.getWidth()/2;
-            double newFaceY = stage.getHeight()/2;
-            leftEye.setCenterX(newFaceX - newFaceRadius * ORIG_EYE_HORIZ_RATIO);
-            leftEye.setCenterY(newFaceY - newFaceRadius * ORIG_EYE_VERT_RATIO);
-            leftEye.setRadius(EYE_TO_FACE_RADIUS_RATIO * newFaceRadius);
-            rightEye.setCenterY(newFaceY - newFaceRadius * ORIG_EYE_VERT_RATIO);
-            rightEye.setCenterX(newFaceX + ORIG_EYE_HORIZ_RATIO * newFaceRadius);
-            rightEye.setRadius(EYE_TO_FACE_RADIUS_RATIO * newFaceRadius);
-            face.setRadius(newFaceRadius);
-            face.setCenterX(newFaceX);
-            face.setCenterY(newFaceY);
-            
-            double fontSize = FONT_TO_FACE_RATIO * newFaceRadius;
-            /* for(int newAttemptValue = newValue.intValue();(newAttemptValue > 0) && (captionSizeTooLarge(captionWithNewFont,scene,face));newAttemptValue--){
-                //test shrinking values for font size to see if they fit
-                newAttemptValue--;
-                fontSize = caption.getFont().getSize() * (double)newAttemptValue/(double)oldValue.intValue();
-                captionWithNewFont.setFont(Font.font("Verdana",fontSize));
-            }
-            captionWithNewFont = null;
-            if(fontSize < 1){
-                fontSize = 1;
-            } */
-            caption.setFont(Font.font("Verdana",fontSize));
-            
-            System.out.println("Caption height: " + caption.getLayoutBounds().getHeight() + " Caption width: " + caption.getLayoutBounds().getWidth() + " Caption font: " + caption.getFont());
-            // not needed: 'scene' width and height
-
-            
-        };
+        
         // add the scene to the stage, then set the title
         stage.setScene(scene);
         stage.setTitle("Smiley Face");
         // show the stage
         stage.show();
-        stage.widthProperty().addListener(stageWidthListener);
-        stage.heightProperty().addListener(stageHeightListener);
+        stage.widthProperty().addListener(stageListener);
+        stage.heightProperty().addListener(stageListener);
     }
 
     public static void main(String[] args) {
